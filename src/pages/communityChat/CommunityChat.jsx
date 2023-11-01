59084a6aff1813320710735a3db86e4b1a07fe3d
Syed Mustassim <smustassim@gmail.com>
2023-11-01 21:17:50 +0530

    feat: community-chat-box

--- src/pages/communityChat/CommunityChat.jsx
+++ src/pages/communityChat/CommunityChat.jsx
@@ -31,24 +31,40 @@ export const CommunityChat = ({setShowNavBar}) =>
   },[])
 
   return (
-    <>
-    <header className="flex flex-col relative overflow-x-hidden ">
-    <Header heading="Home" padding={"pb-14"}/>
-    <section className=" absolute bottom-3 ml-2 flex justify-around items-center w-full font-normal ">
-    <svg width="25" height="23" viewBox="0 0 25 23" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={()=>clickHandler()}>
-    <path d="M25 12.2812H3.05176L12.6587 21.8882L11.5601 22.9868L0.0732422 11.5L11.5601 0.0131836L12.6587 1.11182L3.05176 10.7188H25V12.2812Z" fill="black"/>
-    </svg>
-    <h1 className="text-2xl">{communityName}</h1>
-    <span>{" "}</span>
+    <div className='flex flex-col'>
+      <header className="flex flex-col fixed top-0 w-full overflow-x-hidden">
+      <Header heading="Home" padding={"pb-14"}/>
+      <section className=" absolute bottom-3 ml-2 flex justify-around items-center w-full font-normal ">
+      <svg width="25" height="23" viewBox="0 0 25 23" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={()=>clickHandler()}>
+      <path d="M25 12.2812H3.05176L12.6587 21.8882L11.5601 22.9868L0.0732422 11.5L11.5601 0.0131836L12.6587 1.11182L3.05176 10.7188H25V12.2812Z" fill="black"/>
+      </svg>
+      <h1 className="text-2xl">{communityName}</h1>
+      <span>{" "}</span>
 
-    </section>
-  </header>
+      </section>
+    </header>
 
-    <div className='w-screen h-screen flex flex-col justify-center items-center relative'>
-      
-      <h1>Community Chat</h1>
+    <div className='w-screen h-screen flex flex-col justify-end relative'>
       {openModal && <ImageModal media={media} setMedia={setMedia} setMediaUrl={setMediaUrl} setOpenModal={setOpenModal}/>}
-      <footer className='w-full px-6 px flex justify-items-stretch  fixed bottom-3'>
+      <div className='mb-[40px]'>
+        <div className='flex items-end justify-end mb-[20px]'>
+          <div className="bg-[#FFCD6C]/50 text-white p-2 rounded-tl-2xl rounded-bl-2xl rounded-br-2xl w-[300px]">
+            <h1 className="text-right text-sm text-[#308886] font-bold">You</h1>
+            <h1 className='text-right text-black font-bold'>Hello. How are you?</h1>  
+          </div>
+            <img src="https://i.postimg.cc/fTYff9yW/urahichi.gif" alt="profile" className='w-10 h-10 rounded-full ml-2'/>
+        </div>
+        {mediaUrl && 
+        <div className='flex items-end justify-end'>
+          <div className='bg-[#FFCD6C]/50 text-white p-2 rounded-tl-2xl rounded-bl-2xl rounded-br-2xl w-[300px]'>
+          <h1 className="text-right text-sm text-[#308886] font-bold mb-[3px]">You</h1>
+            <img src={mediaUrl} alt="upload" className="rounded-tl-2xl rounded-bl-2xl rounded-br-2xl"/>
+          </div>
+          <img src="https://i.postimg.cc/fTYff9yW/urahichi.gif" alt="profile" className='w-10 h-10 rounded-full ml-2'/>
+        </div>
+        }
+      </div>
+      <footer className='w-full px-6 px flex justify-items-stretch  sticky bottom-3'>
         <input placeholder='Message' className='border box-border border-black rounded-lg px-4 py-3  flex-1' /> 
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" className="absolute right-11 top-3 cursor-pointer">
           <path d="M4.99994 12L4.39594 6.563C4.22294 5.007 5.82494 3.864 7.23994 4.535L19.1839 10.193C20.7089 10.915 20.7089 13.085 19.1839 13.807L7.23994 19.465C5.82494 20.135 4.22294 18.993 4.39594 17.437L4.99994 12ZM4.99994 12H11.9999" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -59,6 +75,6 @@ export const CommunityChat = ({setShowNavBar}) =>
 
       </footer>
     </div>
-    </>
+    </div>
   )
 }
