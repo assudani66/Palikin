e3493900ce7e4e8da2106f5dcc7e3e8b67faffc1
Syed Mustassim <smustassim@gmail.com>
2023-11-01 20:13:02 +0530

    fix: modal-styling

--- src/pages/communityChat/ImageModal.jsx
+++ src/pages/communityChat/ImageModal.jsx
@@ -15,11 +15,13 @@ export const ImageModal = ({media, setMedia, setMediaUrl, setOpenModal}) => {
         }
       };
     return(
-        <div className="w-[200px] h-[200px] bg-slate-400">
-            <p onClick={() => setOpenModal(false)}>X</p>
-            <h1>Upload image</h1>
-            <input type="file" accept="image/* video/*" className="" onChange={(e) => setMedia(e.target.files[0])}/>
-            <button onClick={(e) => submitHandler(e)}>Upload</button>
+        <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-gray-300/50 z-50">
+            <div className="w-80 h-60 bg-slate-400 p-4 rounded-lg text-white relative">
+                <button onClick={() => setOpenModal(false)} className="absolute top-4 right-4 text-[30px] cursor-pointer">&#10005;</button>
+                <h1 className="text-[30px] font-bold mb-4">Upload image</h1>
+                <input type="file" accept="image/* video/*" className="border p-2 rounded w-full mb-4" onChange={(e) => setMedia(e.target.files[0])}/>
+                <button onClick={(e) => submitHandler(e)} className="bg-blue-500 text-white py-2 px-4 rounded cursor-pointer">Upload</button>
+            </div>
         </div>
     )
 }
