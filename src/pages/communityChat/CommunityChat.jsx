72ff191fe05f44d44eab3bf0d2337da9b74e420a
Wadad Parker <wadadparker5@gmail.com>
2023-11-01 16:49:48 +0530

    style: community chat structure

--- src/pages/communityChat/CommunityChat.jsx
+++ src/pages/communityChat/CommunityChat.jsx
@@ -1,21 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
 
 import { Header } from '../../components';
 
 export const CommunityChat = ({setShowNavBar}) => 
 {
-
+ 
   const communityName = "Harmony Haven"
+
+  const navigate = useNavigate();
+
+  const clickHandler=()=>
+  {
+    setShowNavBar(true);
+    navigate("/communities")
+    
+  }
 
   useEffect(()=>{
     setShowNavBar(false);
-  })
+  },[])
 
   return (
     <><header className="flex flex-col relative overflow-x-hidden ">
     <Header heading="Home" padding={"pb-14"}/>
     <section className=" absolute bottom-3 ml-2 flex justify-around items-center w-full font-normal ">
-    <svg width="25" height="23" viewBox="0 0 25 23" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={()=>navigate("/communities")}>
+    <svg width="25" height="23" viewBox="0 0 25 23" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={()=>clickHandler()}>
     <path d="M25 12.2812H3.05176L12.6587 21.8882L11.5601 22.9868L0.0732422 11.5L11.5601 0.0131836L12.6587 1.11182L3.05176 10.7188H25V12.2812Z" fill="black"/>
     </svg>
     <h1 className="text-2xl">{communityName}</h1>
